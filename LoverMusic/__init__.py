07207f441f90c9e045114bd26b21dcde74c8e134
Lover-Music <123320837+Lover-Music@users.noreply.github.com>
2025-01-08 20:14:33 +0530

    Update __init__.py

--- LoverMusic/__init__.py
+++ LoverMusic/__init__.py
@@ -1,4 +1,4 @@
-from LoverMusic.core.bot import LoverMusic
+from LoverMusic.core.bot import Anon
 from LoverMusic.core.dir import dirr
 from LoverMusic.core.git import git
 from LoverMusic.core.userbot import Userbot
